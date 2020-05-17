3fc691c98da2e085b0afd1d42c200fa343c556bc
Ben Fiola <benfiola@gmail.com>
2020-05-17 15:36:49 -0700

    Move libc/kernel files into src directory

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
-SYSTEM_HEADER_PROJECTS="libc kernel"
-PROJECTS="libc kernel"
+SYSTEM_HEADER_PROJECTS="src/libc src/kernel"
+PROJECTS="src/libc src/kernel"
 
 export MAKE=${MAKE:-make}
 export HOST=${HOST:-$(./default-host.sh)}
