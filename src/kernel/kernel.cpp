949eda4c5ff2ea4a8b41b767e6a4420b8efc9b60
Ben Fiola <benfiola@gmail.com>
2020-05-27 00:16:59 -0700

    Finished

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,11 +1,14 @@
 
 #include <kernel/kernel.h>
 #include <kernel/terminal.h>
+#include <stdint.h>
 
 void kernel_main() {
     Terminal::Initialize();
 
-    for(int x = 0; x < 80; x++) {
-        Terminal::Write((char *) "hello");
+    for(uint8_t x = (int)'A'; x <= (int)'z'; x++) {
+        char c = (char)x;
+        Terminal::Write(&c);
     }
 }
+
